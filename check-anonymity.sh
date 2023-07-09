bc5824f5d0badb7e59e5eacc3a0f2aece8697442
pmbittner <paul.bittner@uni-ulm.de>
2023-07-09 16:51:50 +0200

    script for checking anonymity

---
+++ check-anonymity.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+names=("Paul" "Bittner" "Alex" "Schult" "Jeff" "Young" "Parisa" "Ataei" "Eric" "Walking" "Martin" "Erwig" "Leo" "Teixeira" "Thomas" "Thüm" "VariantSync" "Ulm" "OSU" "Oregon" "GPCE")
+for name in "${names[@]}";
+do
+  # -I ignores binary files
+  grep -r -n -I \
+    --exclude-dir=_build \
+    --exclude-dir=.git \
+    --exclude=check-anonymity.sh \
+    $name
+done
+
