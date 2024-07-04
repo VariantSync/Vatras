db9db9d9200ed88111345b067285168ab68be81a
Benjamin Moosherr <benjamin.moosherr@uni-ulm.de>
2024-07-04 23:26:59 +0200

    Integrate find-inconsistency-sources.sh into CI

--- scripts/find-inconsistency-sources.sh
+++ scripts/find-inconsistency-sources.sh
@@ -1,82 +1,183 @@
 #!/usr/bin/env bash
 
 cd "$(dirname ${BASH_SOURCE[0]})/.."
+export POSIXLY_CORRECT=1
 
-possible-inconsistencies() {
-  echo 'postulates:'
-  grep 'postulate' -r src --include '*agda*'
+awkDocumentationMap='
+    function document(count, line, documentation) {
+      doc[count ":" line]=documentation
+    }
 
-  echo
-  echo 'terminating pragmas:'
-  grep 'TERMINATING' -r src --include '*agda*'
-
-  echo
-  echo 'holes:'
-  (
-    grep -F '{!' -r src --include '*agda*'
-    grep ' ?\( \|$\)' -r src --include '*agda*'
-  )
-}
-
-with-doc() {
-  # Used for alignment.
-  # Cannot use 'column' because it doesn't exist on MAC.
-  columnCount="$(possible-inconsistencies | awk 'BEGIN { max=0 } { if (max < length($0)) max=length($0) } END { print max }')"
-
-  POSIXLY_CORRECT=1 awk '
     BEGIN {
-      doc[""]=""
-      doc["postulates:"]=""
-      doc["terminating pragmas:"]=""
-      doc["holes:"]=""
+      ignore[""]=1
+      ignore["postulates:"]=1
+      ignore["terminating pragmas:"]=1
+      ignore["holes:"]=1
 
-      doc["src/Util/String.agda:  where postulate trustMe : _"]="Also proposed in the Agda issue tracker to make String propositions provable. Only used in examples."
-      # Note: this above postulate appears four times
-      doc["src/Show/Lines.agda:{-# NON_TERMINATING #-}"]="Only used for printing and thus not proof relevant. Also, NON_TERMINATING functions do not reduce during type checking."
-      doc["src/Tutorial/A_NewLanguage.lagda.md:We are using the `{-# TERMINATING -#}` flag here:"]="This is actually a comment."
-      doc["src/Tutorial/A_NewLanguage.lagda.md:{-# TERMINATING #-}"]="Simplification of the tutorial. Not used in the anything else."
-      doc["src/Tutorial/A_NewLanguage.lagda.md:MyConfig = {!!}"]="Intentional hole to be filled by you! Not used in anything other than the tutorial."
-      doc["src/Tutorial/A_NewLanguage.lagda.md:⟦_⟧ = {!!}"]="Intentional hole to be filled by you! Not used in anything other than the tutorial."
-      doc["src/Tutorial/B_Translation.lagda.md:conf e c²ᶜᶜ = {!!}"]="Intentional hole to be filled by you! Not used in anything other than the tutorial."
-      doc["src/Tutorial/B_Translation.lagda.md:fnoc e cᵐʸ = {!!}"]="Intentional hole to be filled by you! Not used in anything other than the tutorial."
-      doc["src/Tutorial/B_Translation.lagda.md:preservation-⊆[] e c = {!!}"]="Intentional hole to be filled by you! Not used in anything other than the tutorial."
-      doc["src/Tutorial/B_Translation.lagda.md:preservation-⊇[] e c = {!!}"]="Intentional hole to be filled by you! Not used in anything other than the tutorial."
-      doc["src/Tutorial/B_Translation.lagda.md:translate (D ⟨ l , r ⟩) = {!!}"]="Intentional hole to be filled by you! Not used in anything other than the tutorial."
-      doc["src/Tutorial/B_Translation.lagda.md:translate (a -< cs >-)  = {!!}"]="Intentional hole to be filled by you! Not used in anything other than the tutorial."
-      doc["src/Tutorial/C_Proofs.lagda.md:  {!!}   -- write down the proof of correctness in this hole"]="Intentional hole to be filled by you! Not used in anything other than the tutorial."
-      doc["src/Tutorial/C_Proofs.lagda.md:  {!!} , -- write down the expression in this hole"]="Intentional hole to be filled by you! Not used in anything other than the tutorial."
-      doc["src/Tutorial/C_Proofs.lagda.md:2CC≽MyLang = {!!}"]="Intentional hole to be filled by you! Not used in anything other than the tutorial."
-      doc["src/Tutorial/C_Proofs.lagda.md:MyLang-is-Sound = {!!}"]="Intentional hole to be filled by you! Not used in anything other than the tutorial."
+      document(1,
+        "src/Util/String.agda:  where postulate trustMe : _",
+        "Also proposed in the Agda issue tracker to make String propositions provable. Only used in examples.")
+      document(2,
+        "src/Util/String.agda:  where postulate trustMe : _",
+        "Also proposed in the Agda issue tracker to make String propositions provable. Only used in examples.")
+      document(3,
+        "src/Util/String.agda:  where postulate trustMe : _",
+        "Also proposed in the Agda issue tracker to make String propositions provable. Only used in examples.")
+      document(4,
+        "src/Util/String.agda:  where postulate trustMe : _",
+        "Also proposed in the Agda issue tracker to make String propositions provable. Only used in examples.")
+      document(1,
+        "src/Show/Lines.agda:{-# NON_TERMINATING #-}",
+        "Only used for printing and thus not proof relevant. Also, NON_TERMINATING functions do not reduce during type checking.")
+      document(1,
+        "src/Tutorial/A_NewLanguage.lagda.md:We are using the `{-# TERMINATING -#}` flag here:",
+        "This is actually a comment.")
+      document(1,
+        "src/Tutorial/A_NewLanguage.lagda.md:{-# TERMINATING #-}",
+        "Simplification of the tutorial. Not used in the anything else.")
+      document(1,
+        "src/Tutorial/A_NewLanguage.lagda.md:MyConfig = {!!}",
+        "Intentional hole to be filled by you! Not used in anything other than the tutorial.")
+      document(1,
+        "src/Tutorial/A_NewLanguage.lagda.md:⟦_⟧ = {!!}",
+        "Intentional hole to be filled by you! Not used in anything other than the tutorial.")
+      document(1,
+        "src/Tutorial/B_Translation.lagda.md:conf e c²ᶜᶜ = {!!}",
+        "Intentional hole to be filled by you! Not used in anything other than the tutorial.")
+      document(1,
+        "src/Tutorial/B_Translation.lagda.md:fnoc e cᵐʸ = {!!}",
+        "Intentional hole to be filled by you! Not used in anything other than the tutorial.")
+      document(1,
+        "src/Tutorial/B_Translation.lagda.md:preservation-⊆[] e c = {!!}",
+        "Intentional hole to be filled by you! Not used in anything other than the tutorial.")
+      document(1,
+        "src/Tutorial/B_Translation.lagda.md:preservation-⊇[] e c = {!!}",
+        "Intentional hole to be filled by you! Not used in anything other than the tutorial.")
+      document(1,
+        "src/Tutorial/B_Translation.lagda.md:translate (D ⟨ l , r ⟩) = {!!}",
+        "Intentional hole to be filled by you! Not used in anything other than the tutorial.")
+      document(1,
+        "src/Tutorial/B_Translation.lagda.md:translate (a -< cs >-)  = {!!}",
+        "Intentional hole to be filled by you! Not used in anything other than the tutorial.")
+      document(1,
+        "src/Tutorial/C_Proofs.lagda.md:  {!!}   -- write down the proof of correctness in this hole",
+        "Intentional hole to be filled by you! Not used in anything other than the tutorial.")
+      document(1,
+        "src/Tutorial/C_Proofs.lagda.md:  {!!} , -- write down the expression in this hole",
+        "Intentional hole to be filled by you! Not used in anything other than the tutorial.")
+      document(1,
+        "src/Tutorial/C_Proofs.lagda.md:2CC≽MyLang = {!!}",
+        "Intentional hole to be filled by you! Not used in anything other than the tutorial.")
+      document(1,
+        "src/Tutorial/C_Proofs.lagda.md:MyLang-is-Sound = {!!}",
+        "Intentional hole to be filled by you! Not used in anything other than the tutorial.")
 
       exitCode=0
     }
+'
 
+possibleInconsistencies() {
+  grepRecursive() {
+    find src -name '*agda*' -exec grep "$@" {} + | sort
+  }
+
+  echo 'postulates:'
+  grepRecursive 'postulate'
+
+  echo
+  echo 'terminating pragmas:'
+  grepRecursive 'TERMINATING'
+
+  echo
+  echo 'holes:'
+  grepRecursive -F '{!'
+  grepRecursive ' ?\( \|$\)'
+}
+
+withDoc() {
+  # Used for alignment.
+  # Cannot use 'column' because it doesn't exist on macOS.
+  columnCount="$(possibleInconsistencies | awk 'BEGIN { max=0 } { if (max < length($0)) max=length($0) } END { print max }')"
+
+  awk -e "$awkDocumentationMap" -e '
     {
       padding='"$columnCount"' - length($0) + 3
-      if ($0 in doc) {
-        printf "%s% *s%s\n", $0, padding, "", doc[$0]
+
+      if ($0 in ignore) {
+        print $0
+      } else if (++counts[$0] ":" $0 in doc) {
+        printf "%s% *s%s\n", $0, padding, "", doc[counts[$0] ":" $0]
       } else {
-        printf "\033[30;41m%s% *s%s\033[39;49m\n", $0, padding, "", "UNDOCUMENTED!!!"
         exitCode=1
+        printf "\033[30;41m%s% *s%s\033[39;49m\n", $0, padding, "", "UNDOCUMENTED!!!"
       }
     }
 
     END {
       exit exitCode
-  }
+    }
+  '
+}
+
+ciMessages() {
+  awk -v FS=: -e "$awkDocumentationMap" -e '
+    {
+      if ($0 in ignore) {
+      } else if (++counts[$0] ":" $0 in doc) {
+        delete doc[counts[$0] ":" $0]
+      } else {
+        exitCode=1
+
+        printf("::error file=%s,line=", $1)
+
+        # Yeah, awk has no builtin escaping
+        gsub("'\''", "'\''\\'\'''\''", $1)
+        pattern=$0
+        gsub("^[^:]*:", "", pattern)
+        gsub("'\''", "'\''\\'\'''\''", pattern)
+
+        system("grep -F -n -e '\''" pattern "'\'' '\''" $1 "'\'' | head -n 1 | sed \"s/:.*/::Undocumented source of inconsistency./\"")
+      }
+    }
+
+    END {
+      for (line in doc) {
+        exitCode=1
+
+        gsub("%", "%25", line)
+        gsub("\\n", "%0A", line)
+        gsub("\"", "\\\"", line)
+
+        documentation=doc[line]
+        gsub("%", "%25", documentation)
+        gsub("\\n", "%0A", documentation)
+        gsub("\"", "\\\"", documentation)
+
+        print "::error file=scripts/find-inconsistency-sources.sh::Obsolete inconsistency documentation:%0Adoc[\"" line "\"]=\"" documentation "\""
+      }
+
+      exit exitCode
+    }
   '
 }
 
 if [ "$1" = "--with-documentation" ]
 then
-  possible-inconsistencies | with-doc
+  possibleInconsistencies | withDoc
+  exitCode=$?
+
+  echo
+  echo "Hint: Use '$0 --with-documentation | less -S' to be able to see documentation to each source and to scroll horizontally"
+elif [ "$1" = "--ci" ]
+then
+  possibleInconsistencies | ciMessages
   exitCode=$?
 else
-  possible-inconsistencies
+  possibleInconsistencies
   exitCode=$?
-fi
 
-echo
-echo "Hint: Use '$0 --with-documentation | less -S' to be able to see documentation to each source and to scroll horizontally"
+  echo
+  echo "Hint: Use '$0 --with-documentation | less -S' to be able to scroll horizontally"
+fi
 
 exit $exitCode
